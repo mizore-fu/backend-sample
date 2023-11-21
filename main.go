c434b8b0e86c09d044ac737ca237a81cf6bd5359
mizore-fu <mhxxname0782@gmail.com>
2023-11-21 13:20:04 +0900

    テストデータを追加

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Task struct {
+	id string
+	name string
+}
+
+func NewTask() *Task {
+	return &Task{}
+}
+
+var tasks []*Task = []*Task{}
+
 func main() {
+	tasks = []*Task{
+		{
+			id: "1",
+			name: "Eat",
+		},
+		{
+			id: "2",
+			name: "Sleep",
+		},
+		{
+			id: "3",
+			name: "Study",
+		},
+	}
+	for _, task := range tasks {
+		fmt.Println(task.name)
+	}
+
 	e := echo.New()
 	e.GET("/", hello)
 	e.Logger.Fatal(e.Start(":8080"))
