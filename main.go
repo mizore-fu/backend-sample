05fd1b477777820cc2c7221177a184e348d06a20
mizore-fu <mhxxname0782@gmail.com>
2023-11-22 10:09:53 +0900

    taskを削除するエンドポイントを追加

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Task struct {
-	Id string `json:"id"`
+	ID string `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -20,22 +20,23 @@ var tasks []*Task = []*Task{}
 func main() {
 	tasks = []*Task{
 		{
-			Id: "1",
+			ID: "1",
 			Name: "Eat",
 		},
 		{
-			Id: "2",
+			ID: "2",
 			Name: "Sleep",
 		},
 		{
-			Id: "3",
+			ID: "3",
 			Name: "Study",
 		},
 	}
 
 	e := echo.New()
 	e.GET("/tasks", GetTasks)
-	e.POST("/add", AddTask)
+	e.POST("/tasks", AddTask)
+	e.DELETE("/tasks/:id", DeleteTask)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
@@ -43,7 +44,7 @@ func GetTasks(c echo.Context) error {
 	return c.JSON(http.StatusOK, tasks)
 }
 
-//http://localhost:8080/add
+//http://localhost:8080/tasks
 //body: {"id":"5", "name": "aaa"}
 func AddTask(c echo.Context) error {
 	newTask := NewTask()
@@ -52,4 +53,25 @@ func AddTask(c echo.Context) error {
 	}
 	tasks = append(tasks, newTask)
 	return c.JSON(http.StatusCreated, newTask)
+}
+
+//http://localhost:8080/tasks/1
+func DeleteTask(c echo.Context) error {
+	id := c.Param("id")
+
+	position := -1
+	for i, task := range tasks {
+		if task.ID == id {
+			position = i
+			break
+		}
+	}
+	if position == -1 {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	tasks[position] = tasks[len(tasks)-1]
+	newTasks := tasks[:len(tasks)-1]
+	tasks = newTasks
+	return c.NoContent(http.StatusNoContent)
 }
